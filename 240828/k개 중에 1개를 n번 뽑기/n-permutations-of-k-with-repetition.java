48bcf99b6d6a54455f3d7b5351a384ef524be2a0
eunalove <88129325+eunalove@users.noreply.github.com>
2024-08-28 23:53:46 +0900

    Title: k개 중에 1개를 n번 뽑기, Time: 190ms, Memory: 18MB, Status: Passed - CodeTree

---
+++ 240828/k개 중에 1개를 n번 뽑기/n-permutations-of-k-with-repetition.java
@@ -0,0 +1,39 @@
+//n과 m
+import java.util.*;
+import java.io.*;
+
+public class Main {
+
+    static StringBuilder sb= new StringBuilder();
+
+    public static void main(String[] args)throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st= new StringTokenizer(br.readLine());
+
+        int k= Integer.parseInt(st.nextToken());
+        int n= Integer.parseInt(st.nextToken());
+
+        int[] ans= new int[n];
+        dfs(0, 1, k, n, ans);
+
+        System.out.print(sb);
+
+    }
+
+    static void dfs(int cnt, int val, int k, int n, int[] ans){
+
+        if(cnt == n){
+            for(int tmp: ans)
+                sb.append(tmp).append(" ");
+
+            sb.append("\n");
+            return;
+        }
+
+        for(int i=1; i<=k; i++){
+            ans[cnt]= i;
+            dfs(cnt+1, val, k, n, ans);
+        }
+    }
+}
