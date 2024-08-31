2cfc93e03be7cf50854825b6085aaa2013ceb71b
eunalove <88129325+eunalove@users.noreply.github.com>
2024-08-31 20:04:44 +0900

    Title: 알파벳과 사칙연산, Time: 120ms, Memory: 9MB, Status: Passed - CodeTree

--- 240831/알파벳과 사칙연산/calculations-with-alphabet.java
+++ 240831/알파벳과 사칙연산/calculations-with-alphabet.java
@@ -9,39 +9,66 @@ public class Main {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         String str= br.readLine();
 
-        Map<Character, Integer> alpa= new HashMap<>();
-        int n= str.length();
+        int[] alpa= new int[6];
 
-        dfs(0, n, str, alpa);
+        for(int i=0; i<str.length(); i+=2){
+            if(str.charAt(i) == 'a') alpa[0]= -1;
+            else if(str.charAt(i) == 'b') alpa[1]= -1;
+            else if(str.charAt(i) == 'c') alpa[2]= -1;
+            else if(str.charAt(i) == 'd') alpa[3]= -1;
+            else if(str.charAt(i) == 'e') alpa[4]= -1;
+            else if(str.charAt(i) == 'f') alpa[5]= -1;
+        }
+
+        dfs(0, str, alpa);
 
         System.out.print(ans);
     }
 
-    static void dfs(int cur, int n, String str, Map<Character, Integer> alpa){
-        if(cur > n){
+    static void dfs(int cur, String str, int[] alpa){
+        if(cur == 6){
 
-            int val= alpa.get(str.charAt(0));
+            int val= 0; char c= str.charAt(0);
+            if(c == 'a') val= alpa[0];
+            else if(c == 'b') val= alpa[1];
+            else if(c == 'c') val= alpa[2];
+            else if(c == 'd') val= alpa[3];
+            else if(c == 'e') val= alpa[4];
+            else if(c == 'f') val= alpa[5];
 
-            for(int i=1; i<n; i+=2){
-                char c= str.charAt(i);
-                int next= alpa.get(str.charAt(i+1));
+            for(int i=1; i<str.length(); i+=2){
+                char cur_c= str.charAt(i);
+                int next=0;
+                c= str.charAt(i+1);
 
-                switch(c){
+                if(c == 'a') next= alpa[0];
+                else if(c == 'b') next= alpa[1];
+                else if(c == 'c') next= alpa[2];
+                else if(c == 'd') next= alpa[3];
+                else if(c == 'e') next= alpa[4];
+                else if(c == 'f') next= alpa[5];
+
+                switch(cur_c){
                     case '+': val += next; break;
                     case '-': val -= next; break;
-                    case '*': val *= next;
+                    case '*': val *= next; break;
                 }
             }
-
             ans= ans> val? ans: val;
             return;
         }
 
-        for(int i=1; i<=4; i++){
-                alpa.put(str.charAt(cur), i);
-                dfs(cur+2, n, str, alpa);
-                alpa.put(str.charAt(cur), i);
+
+        if (alpa[cur] != -1) {
+            dfs(cur + 1, str, alpa);
+        } else {
+            for (int i = 1; i <= 4; i++) {
+                alpa[cur] = i;
+                dfs(cur + 1, str, alpa);
+                alpa[cur] = -1;
             }
+        }
+    
     }
     
 }
