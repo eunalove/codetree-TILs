43e32626a002b5a2ff902d83bd3fa8dfefd031fe
eunalove <88129325+eunalove@users.noreply.github.com>
2024-09-07 02:30:41 +0900

    Title: 단순한 동전 챙기기, Time: 229ms, Memory: 14MB, Status: Wrong Answer - CodeTree

--- 240907/단순한 동전 챙기기/collect-coins-easy.java
+++ 240907/단순한 동전 챙기기/collect-coins-easy.java
@@ -11,14 +11,15 @@ public class Main {
             this.num = num;
         }
     }
+
     static int ans = Integer.MAX_VALUE;
+    static List<Coin> list = new ArrayList<>();
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int n = Integer.parseInt(br.readLine());
-        int sx = 0; int sy = 0;
-        int ex = 0; int ey= 0;
-        List<Coin> list= new ArrayList<>();
+        int sx = 0, sy = 0;
+        int ex= 0; int ey= 0;
 
         for(int i=0; i<n; i++) {
             String str = br.readLine();
@@ -27,31 +28,33 @@ public class Main {
                 if(cur == '.') continue;
 
                 if(cur == 'S') {
-                    sx = i;
-                    sy = j;
+                    sx= i;
+                    sy= j;
                 }
                 else if(cur == 'E') {
-                    ex = i;
-                    ey = j;
+                    ex= i;
+                    ey= j;
                 }
                 else list.add(new Coin(i, j, (int)cur - '0'));
                 
             }
         }
 
-        list.sort((l1, l2)->{
-            return l1.num - l2.num;
+        list.sort((l1, l2)-> {
+            return l1.num = l2.num;
         });
 
-        subset(0, 0, 0, sx, sy, ex, ey, list);
-        ans= ans == Integer.MAX_VALUE? -1: ans;
-        System.out.println(ans);
-    
+        subset(sx, sy, 0, 0, 0, ex, ey);
+        if(ans == Integer.MAX_VALUE)
+            System.out.println(-1);
+        else
+            System.out.println(ans);
     }
-    public static void subset(int cur, int cnt, int idx, int x, int y, int ex, int ey, List<Coin> list) {
+
+    public static void subset(int x, int y, int cur, int cnt, int idx, int ex, int ey) {
 
         if(cur == 3) {
-            int cal = calculate(x, y, ex, ey);
+            int cal = calculate(ex, ey, x, y);
             ans = Math.min(ans, cnt + cal);
             return;
         }
@@ -63,12 +66,12 @@ public class Main {
         Coin c = list.get(idx);
         int ch = calculate(c.x, c.y, x, y);
         // 동전을 선택한 경우
-        subset(cur+1, cnt+ch, idx+1, x, y, ex, ey, list);
+        subset(c.x, c.y, cur+1, cnt+ch, idx+1, ex, ey);
         // 동전을 선택하지 않은 경우
-        subset(cur, cnt, idx+1, x, y, ex, ey, list);
+        subset(x, y, cur, cnt, idx+1, ex, ey);
     }
 
-    public static int calculate(int r, int c, int nr, int nc) {
+    public static int calculate(int nr, int nc, int r, int c) {
 
         return Math.abs(r - nr) + Math.abs(c - nc);
     }
