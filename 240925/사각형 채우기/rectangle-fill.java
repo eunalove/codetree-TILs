cf5dc42f8a649dcad2758d4f386e27a08e69c194
eunalove <88129325+eunalove@users.noreply.github.com>
2024-09-25 00:07:45 +0900

    Title: 사각형 채우기, Time: 92ms, Memory: 8MB, Status: Passed - CodeTree

--- 240925/사각형 채우기/rectangle-fill.java
+++ 240925/사각형 채우기/rectangle-fill.java
@@ -7,17 +7,15 @@ public class Main {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int n= Integer.parseInt(br.readLine());
 
-        int[] dp= new int[n];
+        int[] dp= new int[n+1];
 
-        if(n==1) System.out.print(1);
-        else{
             dp[0]= 1;
-            dp[1]= 2;
+            dp[1]= 1;
 
-            for(int i=2; i<n; i++)
-                dp[i]= dp[i-1]%10007+ dp[i-2]%10007;
+            for(int i=2; i<=n; i++)
+                dp[i]= (dp[i-1]+ dp[i-2])%10007;
 
-            System.out.print(dp[n-1]%10007);
-        }
+            System.out.print(dp[n]);
+        
     }
 }
