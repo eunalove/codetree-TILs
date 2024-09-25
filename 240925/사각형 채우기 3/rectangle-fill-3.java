4e277da55c1ce3d7f1350891c09f604ff40f003c
eunalove <88129325+eunalove@users.noreply.github.com>
2024-09-25 23:46:31 +0900

    Title: 사각형 채우기 3, Time: 143ms, Memory: 10MB, Status: Passed - CodeTree

--- 240925/사각형 채우기 3/rectangle-fill-3.java
+++ 240925/사각형 채우기 3/rectangle-fill-3.java
@@ -1,24 +1,32 @@
-import java.util.*;
-import java.io.*;
+import java.util.Scanner;
 
 public class Main {
-    public static void main(String[] args)throws IOException {
+    public static final int MOD = 1000000007;
+    public static final int MAX_N = 1000;
+    
+    // 변수 선언
+    public static int n;
+    
+    public static long[] dp = new long[MAX_N + 1];
 
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int n= Integer.parseInt(br.readLine());
-        
-            int[] dp= new int[n+1];
-            
-            dp[0]= 1;
-            dp[1]= 2;
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        // 입력:
+        n = sc.nextInt();
 
-            for(int i=2; i<=n; i++){
-                dp[i]= (dp[i-1] *2 + dp[i-2]*3)%1000000007;
-                for(int j= i-3; j>=0; j--)
-                    dp[i]= (dp[i]+ dp[j] *2) %1000000007;
-            }
-               
-            System.out.print(dp[n]);
+        // 초기 조건 설정
+        dp[0] = 1;
+        dp[1] = 2;
+
+        // 점화식에 따라 dp값 채우기
+        // dp[i] = dp[i - 1] * 2 + dp[i - 2] * 3 +
+        //         (dp[i - 3] + dp[i - 4] + dp[i - 5] + ... dp[0]) * 2
+        for(int i = 2; i <= n; i++) {
+            dp[i] = (dp[i - 1] * 2 + dp[i - 2] * 3) % MOD;
+            for(int j = i - 3; j >= 0; j--)
+                dp[i] = (dp[i] + dp[j] * 2) % MOD;
+        }
         
+        System.out.print(dp[n]);
     }
 }
