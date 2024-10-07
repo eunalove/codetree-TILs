b656280c6fc4809b9e256325eb3c75ce9b0e8095
eunalove <88129325+eunalove@users.noreply.github.com>
2024-10-07 23:10:42 +0900

    Title: 사각형 채우기 2, Time: 100ms, Memory: 8MB, Status: Wrong Answer - CodeTree

---
+++ 241007/사각형 채우기 2/rectangle-fill-2.java
@@ -0,0 +1,23 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args)throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n= Integer.parseInt(br.readLine());
+        
+        int[] dp= new int[1000];
+
+        dp[0]= 1;
+        dp[1]= 3;
+        dp[2]= 10;
+
+        for(int i=3; i<n; i++)
+            dp[i]= dp[i-1] + dp[i-2]+ dp[i-3];
+
+        if(n < 3) System.out.print(dp[n-1]);
+        else System.out.print(dp[n-1]+1);
+
+    }
+}
