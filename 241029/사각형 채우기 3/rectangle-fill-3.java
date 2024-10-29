adc1cb04bd30aaf14235e5298a0e67dd710924c8
eunalove <88129325+eunalove@users.noreply.github.com>
2024-10-29 18:15:15 +0900

    Title: 사각형 채우기 3, Time: 117ms, Memory: 10MB, Status: Wrong Answer - CodeTree

--- 241029/사각형 채우기 3/rectangle-fill-3.java
+++ 241029/사각형 채우기 3/rectangle-fill-3.java
@@ -13,7 +13,7 @@ public class Main {
         dp[2]= 7;
 
         for(int i=3; i<=n; i++)
-            dp[i]= (dp[i-1]*2 + dp[i-2]*3 + 2*(i-2))%1000000007;
+            dp[i]= (dp[i-1]*2 + dp[i-2]*3 + 2)%1000000007;
 
         System.out.print(dp[n]);
 
