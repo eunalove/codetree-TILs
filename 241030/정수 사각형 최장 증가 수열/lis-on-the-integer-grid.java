8697654f096ce061e588482bf9f56d1ad5b9e499
eunalove <88129325+eunalove@users.noreply.github.com>
2024-10-30 19:16:49 +0900

    Title: 정수 사각형 최장 증가 수열, Time: 994ms, Memory: 30MB, Status: Passed - CodeTree

--- 241030/정수 사각형 최장 증가 수열/lis-on-the-integer-grid.java
+++ 241030/정수 사각형 최장 증가 수열/lis-on-the-integer-grid.java
@@ -1,6 +1,16 @@
 import java.util.*;
 import java.io.*;
 
+class Cell{
+    int x, y, num;
+
+    public Cell(int x, int y, int num){
+        this.x= x;
+        this.y= y;
+        this.num= num;
+    }
+}
+
 public class Main {
     
     static int[] dx= {-1, 1, 0, 0};
@@ -21,21 +31,32 @@ public class Main {
             }
         }
 
+        List<Cell> cells= new ArrayList<>();
+
+        for(int i=0; i<n; i++)
+            for(int j=0; j<n; j++)
+                cells.add(new Cell(i, j, map[i][j]));
+        
+        Collections.sort(cells, (c1, c2)->{
+            return c1.num- c2.num;
+        });
+
         int[][] dp= new int[n][n];
 
-        for(int i=0; i<n; i++){
-            for(int j=0; j<n; j++){
-                
-                for(int k=0; k<4; k++){
-                    int nx= i+ dx[k];
-                    int ny= j+ dy[k];
+        for(Cell cell: cells){
+            int x= cell.x;
+            int y= cell.y;
+            int num= cell.num;
 
-                    if(nx<0 || ny<0 || nx>=n || ny>=n) continue;
+            for(int k=0; k<4; k++){
+                int nx= x+ dx[k];
+                int ny= y+ dy[k];
 
-                    if(map[i][j] < map[nx][ny])
-                        dp[nx][ny]= Math.max(dp[nx][ny], dp[i][j]+1);
+                if(nx<0 || ny<0 || nx>=n || ny>=n) continue;
+
+                if(map[x][y] < map[nx][ny])
+                    dp[nx][ny]= Math.max(dp[nx][ny], dp[x][y]+1);
                 }
-            }
         }
 
 
