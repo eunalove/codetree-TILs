755daa539e79683ac89eeacb0aed9894a6196c82
eunalove <88129325+eunalove@users.noreply.github.com>
2024-10-30 20:30:07 +0900

    Title: 최대 증가 부분 수열, Time: 100ms, Memory: 9MB, Status: Wrong Answer - CodeTree

--- 241030/최대 증가 부분 수열/longest-increasing-subsequence.java
+++ 241030/최대 증가 부분 수열/longest-increasing-subsequence.java
@@ -15,7 +15,7 @@ public class Main {
         
         int[] dp= new int[1001];
 
-        for(int i=0; i<n; i++){
+        for(int i=1; i<n; i++){
             for(int j= i-1; j>=0; j--)
                 if(arr[i] > arr[j]){
                     dp[i]= dp[j]+1;
