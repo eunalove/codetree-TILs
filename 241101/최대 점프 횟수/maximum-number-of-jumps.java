3417f33779bd32b832d32cc51b1d97295f2241a7
eunalove <88129325+eunalove@users.noreply.github.com>
2024-11-01 23:59:46 +0900

    Title: 최대 점프 횟수, Time: 172ms, Memory: 11MB, Status: Passed - CodeTree

--- 241101/최대 점프 횟수/maximum-number-of-jumps.java
+++ 241101/최대 점프 횟수/maximum-number-of-jumps.java
@@ -1,32 +1,42 @@
-import java.util.*;
-import java.io.*;
+import java.util.Scanner;
 
 public class Main {
-    public static void main(String[] args)throws IOException {
-
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int n = Integer.parseInt(br.readLine());
-
-        StringTokenizer st = new StringTokenizer(br.readLine());
-        int[] arr = new int[n];
-
-        for (int i = 0; i < n; i++) {
-            arr[i] = Integer.parseInt(st.nextToken());
-        }
-
-        int[] dp = new int[n];
-        Arrays.fill(dp, Integer.MAX_VALUE);
+    public static final int INT_MIN = Integer.MIN_VALUE;
+    public static final int MAX_N = 1000;
+    
+    // 변수 선언
+    public static int n;
+    public static int[] arr = new int[MAX_N];
+    
+    public static int[] dp = new int[MAX_N];
+    
+    public static void initialize() {
+        for(int i = 0; i < n; i++)
+            dp[i] = INT_MIN;
         dp[0] = 0;
+    }
 
-        for (int i = 0; i < n; i++) {
-            if (dp[i] == Integer.MAX_VALUE) continue;
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        // 입력: 
+        n = sc.nextInt();
+        for(int i = 0; i < n; i++)
+            arr[i] = sc.nextInt();
+        initialize();
 
-            for (int j = 1; j <= arr[i] && i + j < n; j++) {
-                dp[i + j] = Math.min(dp[i + j], dp[i] + 1);
+        for(int i = 1; i < n; i++) {
+            for(int j = 0; j < i; j++) {
+                if(dp[j] == INT_MIN)
+                    continue;
+                
+                if(j + arr[j] >= i)
+                    dp[i] = Math.max(dp[i], dp[j] + 1);
             }
         }
-
-        int ans = dp[n - 1] == Integer.MAX_VALUE ? -1 : dp[n - 1];
-        System.out.println(ans);
+        int ans = INT_MIN;
+        for(int i = 0; i < n; i++)
+            ans = Math.max(ans, dp[i]);
+        
+        System.out.print(ans);
     }
 }
