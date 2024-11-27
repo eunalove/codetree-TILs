d6c1570961132ebc38eb12e8ba419e4fbfe5daeb
eunalove <88129325+eunalove@users.noreply.github.com>
2024-11-27 23:31:56 +0900

    Title: 주사위 던지기 2, Time: 113ms, Memory: 11MB, Status: Passed - CodeTree

---
+++ 241127/주사위 던지기 2/roll-dice-2.java
@@ -0,0 +1,45 @@
+//n과 m
+import java.util.*;
+import java.io.*;
+
+public class Main {
+
+    static StringBuilder sb= new StringBuilder();
+
+    public static void main(String[] args)throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st= new StringTokenizer(br.readLine());
+
+        int n= Integer.parseInt(st.nextToken());
+        int m= Integer.parseInt(st.nextToken());
+
+        int[] arr= new int[n];
+
+        //중복순열
+        permul(0, 0, n, m, arr);
+
+        System.out.print(sb);
+
+    }
+
+    static void permul(int cur, int sum, int n, int m, int[] arr){
+
+        if(cur == n && sum == m){
+            for(int element: arr)
+                sb.append(element).append(" ");
+
+            sb.append("\n");
+
+            return;
+        }
+
+        if(cur >= n || sum > m) return;
+
+        for(int i=1; i<=6; i++){
+            arr[cur]= i;
+            permul(cur+1, sum+i, n, m, arr);
+        }
+
+    }
+}
