1ff364064aaafcd7234294de8b459809131390de
eunalove <88129325+eunalove@users.noreply.github.com>
2025-02-06 17:49:23 +0900

    Title: 트로미노, Time: 143ms, Memory: 10MB, Status: Passed - CodeTree

--- 250206/트로미노/tromino.java
+++ 250206/트로미노/tromino.java
@@ -1,50 +1,50 @@
-//n과 m
 import java.util.*;
 import java.io.*;
 
 public class Main {
     public static void main(String[] args)throws IOException {
-
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st= new StringTokenizer(br.readLine());
 
-        int n= Integer.parseInt(st.nextToken());
-        int m= Integer.parseInt(st.nextToken());
+        int n = Integer.parseInt(st.nextToken());
+        int m = Integer.parseInt(st.nextToken());
 
-        int[][] map= new int[n+2][m+2];
+        int[][] map = new int[n + 2][m + 2];
 
-        for(int i=0; i<n+2; i++){
-            for(int j=0; j<m+2; j++){
-                map[i][j]= Integer.MIN_VALUE;
+        // 경계값을 Integer.MIN_VALUE로 초기화
+        for(int i = 0; i < n + 2; i++){
+            for(int j = 0; j < m + 2; j++){
+                map[i][j] = Integer.MIN_VALUE;
             }
         }
 
-        for(int i=1; i<=n; i++){
-            st= new StringTokenizer(br.readLine());
-            for(int j=1; j<=m; j++){
-                map[i][j]= Integer.parseInt(st.nextToken());
+        // 실제 값 입력
+        for(int i = 1; i <= n; i++){
+            st = new StringTokenizer(br.readLine());
+            for(int j = 1; j <= m; j++){
+                map[i][j] = Integer.parseInt(st.nextToken());
             }
         }
 
-        int ans= 0;
+        int ans = Integer.MIN_VALUE; // 결과값을 가장 작은 값으로 초기화
 
-        for(int i=1; i<=n; i++){
-            for(int j=1; j<=m; j++){
+        // 가능한 모든 3칸 조합 확인
+        for(int i = 1; i <= n; i++){
+            for(int j = 1; j <= m; j++){
 
-                int max= 0;
-                max= map[i-1][j]+ map[i][j+1] +map[i][j] > max? map[i-1][j]+ map[i][j+1]+map[i][j] : max;
-                max= map[i+1][j]+ map[i][j+1] +map[i][j]> max? map[i+1][j]+ map[i][j+1]+map[i][j] : max;
-                max= map[i-1][j]+ map[i][j-1] +map[i][j]> max? map[i-1][j]+ map[i][j-1]+map[i][j] : max;
-                max= map[i-1][j]+ map[i][j-1] +map[i][j]> max? map[i-1][j]+ map[i][j-1]+map[i][j] : max;
-                max= map[i+1][j]+ map[i-1][j] +map[i][j]> max? map[i+1][j]+ map[i-1][j]+map[i][j] : max;
-                max= map[i][j-1]+ map[i][j+1] +map[i][j]> max? map[i][j-1]+ map[i][j+1]+map[i][j] : max;
+                int max = Integer.MIN_VALUE;
 
-                ans= ans> max? ans: max;
+                max = Math.max(max, map[i-1][j] + map[i][j+1] + map[i][j]); // 위 + 오른쪽
+                max = Math.max(max, map[i+1][j] + map[i][j+1] + map[i][j]); // 아래 + 오른쪽
+                max = Math.max(max, map[i-1][j] + map[i][j-1] + map[i][j]); // 위 + 왼쪽
+                max = Math.max(max, map[i+1][j] + map[i][j-1] + map[i][j]); // 아래 + 왼쪽
+                max = Math.max(max, map[i+1][j] + map[i-1][j] + map[i][j]); // 위 + 아래
+                max = Math.max(max, map[i][j-1] + map[i][j+1] + map[i][j]); // 왼쪽 + 오른쪽
 
+                ans = Math.max(ans, max);
             }
         }
 
         System.out.print(ans);
-
     }
 }
